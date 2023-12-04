fec96d66776d2e3d8460e0282bb8b01dd86e5dda
KevinAG23 <guzman.agk1523@gmail.com>
2023-12-03 22:41:14 -0500

    Subir Archivo

---
+++ UNO_CORE/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//========= PRIMERO =========
+var misReglasCors = "ReglasCors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: misReglasCors,
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                           
+
+                      });
+});
+//builder.SetIsOriginAllowed(x => true)
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//========= SEGUNDO =========
+app.UseCors(misReglasCors);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
